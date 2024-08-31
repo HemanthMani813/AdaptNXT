f79c211b1adaa4e09b04efd77314e175f60c7ca5
HemanthMani813 <hemanth.mani0813@gmail.com>
2024-08-31 13:26:59 +0530

    Assignment

--- src/components/SalesChart/index.js
+++ src/components/SalesChart/index.js
@@ -42,28 +42,6 @@ const SalesChart=()=>{
                 </LineChart>
         </ResponsiveContainer>
         </div>
-    //     <ResponsiveContainer width={800} height={500}>
-    //     <LineChart
-    //       width={500}
-    //       height={300}
-    //       data={data}
-    //       margin={{
-    //         top: 5,
-    //         right: 30,
-    //         left: 20,
-    //         bottom: 5,
-    //       }}
-    //     >
-    //       <CartesianGrid strokeDasharray="3 3" />
-    //       <XAxis dataKey="name" />
-    //       <YAxis yAxisId="left" />
-    //       <YAxis yAxisId="right" orientation="right" />
-    //       <Tooltip />
-    //       <Legend />
-    //       <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-    //       <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
-    //     </LineChart>
-    //   </ResponsiveContainer>
     )
 }
 export default SalesChart
